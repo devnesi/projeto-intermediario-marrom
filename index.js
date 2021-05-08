4fe369513d1ff042dd7427146ecc2e25ef9c008b
devnesi <nesilovegood@gmail.com>
2021-05-07 22:11:07 -0300

    add query string com paginacao

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ app.get('/olx/terrenos', (req, res) => {
     const retorno = {}
     const lotes = []
     var page = req.query.page
+    var q = req.query.q
+    
     page = page === undefined ? 'https://sc.olx.com.br/florianopolis-e-regiao/outras-cidades/criciuma/imoveis/terrenos' : 'https://sc.olx.com.br/florianopolis-e-regiao/outras-cidades/criciuma/imoveis/terrenos?o=' + page
     axios.get(page).then(response => {
         const doc = new dom().parseFromString(response.data)
         const nextpage = xpath.select('//a[@data-lurker-detail="next_page"]/@href', doc)[0].value
 
         if (nextpage != undefined) {
-            retorno.proxima = 'http://localhost:3000/olx/terrenos?page=' + nextpage.split('?o=')[1]
-            retorno.anterior = 'http://localhost:3000/olx/terrenos?page=' + Number(nextpage.split('?o=')[1] - 1)
+            retorno.proxima = 'http://localhost:3000/olx/terrenos?page=' + nextpage.split('?o=')[1] + (q != undefined? '&q=' + q : '')
+            retorno.anterior = 'http://localhost:3000/olx/terrenos?page=' + Number(nextpage.split('?o=')[1] - 1) + (q != undefined? '&q=' + q : '')
         }
 
         const items = xpath.select('//ul[@id="ad-list"]/li', doc)
@@ -29,7 +31,17 @@ app.get('/olx/terrenos', (req, res) => {
                 image: xpath.select('.//img/@src', item)[0].value,
             }
 
-            lotes.push(lote)
+            if(q != undefined){
+                if(lote.title.includes(q)){
+                    lotes.push(lote)
+                }
+            } else {
+                lotes.push(lote)
+            }
+            
+            
+
+            
         })
         retorno.results = lotes
         res.send(retorno)
@@ -39,7 +51,8 @@ app.get('/olx/terrenos', (req, res) => {
 
 app.get('/olx/carros', (req, res) => {
     const retorno = {}
-    const lotes = []
+    const carros = []
+    var q = req.query.q
     var page = req.query.page
     page = page === undefined ? 'https://sc.olx.com.br/florianopolis-e-regiao/outras-cidades/criciuma/autos-e-pecas/carros-vans-e-utilitarios' : 'https://sc.olx.com.br/florianopolis-e-regiao/outras-cidades/criciuma/autos-e-pecas/carros-vans-e-utilitarios?o=' + page
     axios.get(page).then(response => {
@@ -47,8 +60,8 @@ app.get('/olx/carros', (req, res) => {
         const nextpage = xpath.select('//a[@data-lurker-detail="next_page"]/@href', doc)[0].value
 
         if (nextpage != undefined) {
-            retorno.proxima = 'http://localhost:3000/olx/carros?page=' + nextpage.split('?o=')[1]
-            retorno.anterior = 'http://localhost:3000/olx/carros?page=' + Number(nextpage.split('?o=')[1] - 1)
+            retorno.proxima = 'http://localhost:3000/olx/carros?page=' + nextpage.split('?o=')[1] + (q != undefined? '&q=' + q : '')
+            retorno.anterior = 'http://localhost:3000/olx/carros?page=' + Number(nextpage.split('?o=')[1] - 1) + (q != undefined? '&q=' + q : '')
         }
 
         const items = xpath.select('//ul[@id="ad-list"]/li', doc)
@@ -56,15 +69,23 @@ app.get('/olx/carros', (req, res) => {
             const href = xpath.select('./a[@data-lurker-detail="list_id"]/@href', item)
             if (href.length == 0) return
 
-            const lote = {
+            const carro = {
                 href: href[0].value,
                 title: xpath.select('./a[@data-lurker-detail="list_id"]/@title', item)[0].value,
                 image: xpath.select('.//img/@src', item)[0].value,
             }
 
-            lotes.push(lote)
+            if(q != undefined){
+                if(carro.title.includes(q)){
+                    carros.push(carro)
+                }
+            } else {
+                carros.push(carro)
+            }
+
+            
         })
-        retorno.results = lotes
+        retorno.results = carros
         res.send(retorno)
     })
 })
